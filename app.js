cfc047ee44d6ac3af2ce24c89e429c6af5b99e83
Bhanu Agrawal <bagrawal@deqode.com>
2019-08-27 17:32:05 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const authRoutes = require('./routes/oauthRoutes');
+const app = express();
+
+//setup view engine
+app.set('view engine', 'ejs');
+
+//setup oauth routes
+app.use('/auth', authRoutes);
+
+//create home route
+app.get('/',(req,res)=>{
+    res.render('home');
+})
+
+app.listen(3000,()=>{
+    console.log("Listening on port 3000");
+})
